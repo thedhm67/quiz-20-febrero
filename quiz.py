b17c3672f804401cf388edc44e12874cd763029e
thedhm67 <diego2251704@correo.uis.edu.co>
2026-02-27 20:25:33 -0500

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -54,11 +54,63 @@ def _eliminar(nodo, cedula, self):
 
 
 def consultar_por_cedula(self, cedula):
-    actual = self.cabeza
-    if nodo is None:
-        nodo=self.cabeza
-    
-    if nodo is None:
-        print("no encontrado")
-        return
-    
+        actual = self.cabeza
+
+        while actual:
+            if actual.cedula == cedula:
+                print("Cédula:", actual.cedula)
+                print("Nombre:", actual.nombre)
+                print("Habitación:", actual.habitacion)
+                print("Orden llegada:", actual.orden)
+                return
+            actual = actual.siguiente
+
+        print("No encontrado.")
+
+ def listar_por_llegada(self):
+        actual = self.cabeza
+        lista = []
+
+        while actual:
+            lista.append(actual)
+            actual = actual.siguiente
+
+        lista.sort(key=lambda x: x.orden)
+
+        for h in lista:
+            print(h.cedula, h.nombre, h.habitacion, h.orden)  
+
+
+
+
+ def habitaciones_disponibles(self):
+        disponibles = []
+
+        for i in range(1, self.total_habitaciones + 1):
+            if i not in self.habitaciones_ocupadas:
+                disponibles.append(i)
+
+        print("Disponibles:", disponibles)
+
+
+ def habitaciones_ocupadas_lista(self):
+        print("Ocupadas:", list(self.habitaciones_ocupadas))
+
+
+
+hotel = Hotel(5)
+
+hotel.registrar_entrada("123", "Juan", 1)
+hotel.registrar_entrada("456", "Ana", 2)
+
+hotel.consultar_por_cedula("123")
+
+hotel.listar_por_llegada()
+
+hotel.habitaciones_disponibles()
+hotel.habitaciones_ocupadas_lista()
+
+hotel.registrar_salida("123")
+
+hotel.habitaciones_disponibles()
+
